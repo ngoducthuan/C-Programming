59924743169c039097ebd8c47813b55e887d4743
unknown <Administrator@stone.local>
2025-08-26 15:10:33 +0700

    Basic Programing

---
+++ Basic Pogram/src/format_in_c.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I am %d years old.\n", 20);
+    return 0;
+}
---
+++ Basic Pogram/src/print_info.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("First Name: Ngo\nLast Name: Thuan\nGender: male\nAge: 21");
+
+    return 0;
+}
